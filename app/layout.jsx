4aae521c1cc19d0e6801cccd4fe8a441387cecea
Sadegh-abdollahian <128248700+Sadegh-abdollahian@users.noreply.github.com>
2024-04-27 20:45:23 +0330

    created tailwindcss setup, created clerk setup

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "linkly",
+  description: "You can short your url and links with our website.",
+  icons: {
+    icon: "",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
